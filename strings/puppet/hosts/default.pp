d9e71eb960bbeb005a60b48dc9ef2c096d6eb4cb
James S. White <whitejs@balder.(none)>
2009-11-03 11:51:26 -0600

    whoops. reboot loop

--- strings/puppet/hosts/default.pp
+++ strings/puppet/hosts/default.pp
@@ -5,5 +5,3 @@ include base::ldap-client
 include base::postfix_should_only_run_locally
 include base::sudoers
 include base::sshkeys
-# why does this have to be included for every system?
-include base::vps::linode::pv_grub
